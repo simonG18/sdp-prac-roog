222e4849939f525c2abc03faf87f3ebdbfece564
Simon_g19s4902 <simongee17@icloud.com>
2021-05-11 01:23:25 +0200

    Modified to print student no- g19s4902

--- Roog/Program.cs
+++ Roog/Program.cs
@@ -6,7 +6,7 @@ namespace Roog
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("g19s4902");
         }
     }
 }
